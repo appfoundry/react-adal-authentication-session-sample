c080a3cabcba946ec9ed07d27a06c74720000fd0
CauseOfDev <ict.debaere@gmail.com>
2018-11-24 14:25:49 +0100

    Added code to redirect the user to Microsoft’s login page or to render the React application after successfully logging in and acquiring a token

--- src/index.js
+++ src/index.js
@@ -9,4 +9,18 @@ import AuthContext from './services/Auth'
 // Handle possible callbacks on id_token or access_token
 AuthContext.handleWindowCallback()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Extra callback logic, only in the actual application, not in iframes in the app
+if ((window === window.parent) && window === window.top && !AuthContext.isCallback(window.location.hash)) {
+  // Having both of these checks is to prevent having a token in localstorage, but no user.
+  if (!AuthContext.getCachedToken(AdalConfig.clientId) || !AuthContext.getCachedUser()) {
+    AuthContext.login()
+    // or render something that everyone can see
+    // ReactDOM.render(<PublicPartOfApp />, document.getElementById('root'))
+  } else {
+    AuthContext.acquireToken(AdalConfig.endpoints.api, (message, token, msg) => {
+      if (token) {
+        ReactDOM.render(<App />, document.getElementById('root'))
+      }
+    })
+  }
+}
