720aae763cbbdf745f25f5ce36805bf6eb5862b3
CauseOfDev <ict.debaere@gmail.com>
2018-11-24 14:20:38 +0100

    Import the AuthContext from our authentication service and import the Adal config to be able to use the IDs

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import AdalConfig from './config/AdalConfig'
+import AuthContext from './services/Auth'
+
 ReactDOM.render(<App />, document.getElementById('root'));
