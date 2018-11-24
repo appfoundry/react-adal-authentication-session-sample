3e52ad7dd3bba73333ca45b59fe2d794f1605f79
CauseOfDev <ict.debaere@gmail.com>
2018-11-24 14:17:59 +0100

    Import Api service and add code to test network requests (and to logout)

--- src/App.js
+++ src/App.js
@@ -2,26 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import AuthContext from './services/Auth'
+import Api from './services/Api'
+
 class App extends Component {
+  componentDidMount() {
+    // Perform a network request on mount to easily test our setup
+    Api.get('/todos')
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={ logo } className="App-logo" alt="logo" />
+          <h1 className="App-title">React Adal Authentication Session Sample</h1>
         </header>
+        <p className="App-intro">
+          You should only be able to view this when you are logged in.
+        </p>
+        <div>
+          <p>
+            Open the browser's developer tools and go to the network tab. Then click this button and see that the access token is in the Authorization header of the network request,
+            and if the backend accepts it.
+          </p>
+          <button onClick={ () => Api.get('/todos') }>Test GET data</button>
+        </div>
+        <div>
+          <p>Press this button to logout.</p>
+          <button onClick={ () => AuthContext.logOut() }>Logout</button>
+        </div>
       </div>
-    );
+    )
   }
 }
 
