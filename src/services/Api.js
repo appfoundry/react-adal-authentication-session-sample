666428694b911df79ea4d88604c24be626cbfd48
CauseOfDev <ict.debaere@gmail.com>
2018-11-24 14:46:37 +0100

    Add interceptor to network requests to re-acquire tokens and add the Bearer token to the Authorization header of the request

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,28 @@
 import axios from 'axios'
 
 import ApiConfig from '../config/ApiConfig'
+import AdalConfig from '../config/AdalConfig'
+import AuthContext from './Auth'
 
 const instance = axios.create(ApiConfig)
+
+// Add a request interceptor
+instance.interceptors.request.use((config) => {
+  // Check and acquire a token before the request is sent
+  return new Promise((resolve, reject) => {
+    AuthContext.acquireToken(AdalConfig.endpoints.api, (message, token, msg) => {
+      if (!!token) {
+        config.headers.Authorization = `Bearer ${token}`
+        resolve(config)
+      } else {
+        // Do something with error of acquiring the token
+        reject(config)
+      }
+    })
+  })
+}, function(error) {
+  // Do something with error of the request
+  return Promise.reject(error)
+})
 
 export default instance
