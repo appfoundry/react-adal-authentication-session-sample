97a4d5e88ef6240a0e982561d6b1ee99c1d5e1c6
CauseOfDev <ict.debaere@gmail.com>
2018-11-24 14:22:57 +0100

    Handle any possible callbacks for the Adal library after logging in or after acquiring tokens

--- src/index.js
+++ src/index.js
@@ -6,4 +6,7 @@ import App from './App';
 import AdalConfig from './config/AdalConfig'
 import AuthContext from './services/Auth'
 
+// Handle possible callbacks on id_token or access_token
+AuthContext.handleWindowCallback()
+
 ReactDOM.render(<App />, document.getElementById('root'));
