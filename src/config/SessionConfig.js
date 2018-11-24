e8723fc2e6e2666e0d3daeb71334726e922b57a4
CauseOfDev <ict.debaere@gmail.com>
2018-11-24 21:03:26 +0100

    Added session configuration file

---
+++ src/config/SessionConfig.js
@@ -0,0 +1,7 @@
+// src/config/SessionConfig.js
+export default {
+  uuid: "ENTER UUID HERE", // replace with your own uuid, for example using https://www.uuidgenerator.net
+  timeoutInMinutes: 30,
+  cacheLocation: 'localStorage',
+  debugMode: true // boolean to show or hide console log statements, useful while developing
+}
